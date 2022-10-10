832a93999414edc884924661bcf8404ef4369de4
Teddy Cubaka <teddycubaka96@gmail.com>
2022-10-10 12:07:06 +0100

    display contact in home component

--- src/components/bigs/home.js
+++ src/components/bigs/home.js
@@ -1,12 +1,12 @@
-import React, { useEffect } from "react";
-import { json } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import Contact from "../basics/contact";
 
 export default function Home (){
+    const [contact, setContact] = useState([])
     useEffect(()=>{
         fetch('http://localhost:3000/api/user')
             .then(data => data.json()
-                .then(res => console.log(res))
+                .then(res => setContact(res))
                 .catch(err => console.log(err))
             )
             .catch(err => console.log(err))
@@ -14,7 +14,13 @@ export default function Home (){
     return (
         <div className="home radius">
             <h3>Recent</h3>
-            <Contact />
+            {
+                contact.length > 1 ? 
+                contact.map((cont)=>(
+                    <Contact name={`${cont.firstName} ${cont.secondName}`} message={false} />
+                ))
+                : <div>No contact here</div>
+            }
         </div>
     )
 }
