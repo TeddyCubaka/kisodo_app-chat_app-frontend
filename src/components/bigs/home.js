21cf4065fa662b95458110461e2af380083f3d6c
Teddy Cubaka <teddycubaka96@gmail.com>
2022-10-10 11:52:51 +0100

    first fecth, user data fetched

--- src/components/bigs/home.js
+++ src/components/bigs/home.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { json } from "react-router-dom";
 import Contact from "../basics/contact";
 
 export default function Home (){
+    useEffect(()=>{
+        fetch('http://localhost:3000/api/user')
+            .then(data => data.json()
+                .then(res => console.log(res))
+                .catch(err => console.log(err))
+            )
+            .catch(err => console.log(err))
+    }, [])
     return (
         <div className="home radius">
             <h3>Recent</h3>
