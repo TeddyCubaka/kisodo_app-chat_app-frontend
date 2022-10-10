32b7d50601bd327c52fad157d512e25bd7a8fcbe
Teddy Cubaka <teddycubaka96@gmail.com>
2022-10-10 17:42:25 +0100

    stock message in the discut context

--- src/components/basics/textZone.js
+++ src/components/basics/textZone.js
@@ -1,19 +1,49 @@
-import React from "react";
-import { AiOutlineCamera, AiOutlineSend } from "react-icons/ai"
+import React, { useContext, useState } from "react";
+import { AiOutlineCamera, AiOutlineSend } from "react-icons/ai";
+import discussionContext from "../../contexts/discussion";
 
-
-export default function TextZone (){
-    return (
-        <div className="text_zone">
-            <div className="text_input">
-                <textarea spellcheck="true"></textarea>
-                <div>
-                    <AiOutlineCamera />
-                </div>
-            </div>
-            <button>
-                <AiOutlineSend />
-            </button>
+export default function TextZone() {
+  const { discut } = useContext(discussionContext);
+  const arr = [];
+  const [value, setValue] = useState("");
+  return (
+    <div className="text_zone">
+      <div className="text_input">
+        <textarea
+          spellCheck="true"
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+          value={value}
+        ></textarea>
+        <div>
+          <AiOutlineCamera size="30px" />
         </div>
-    )
+      </div>
+      <button
+        onClick={() => {
+          if (value !== "") {
+            const message = {
+              content: value,
+              isPicture: false,
+              delete: false,
+              vue: false,
+              sendDate: new Date().toLocaleString(),
+              sender: {
+                userId: "",
+                fullName: "",
+                image: "",
+                isOnline: true,
+                biography: "",
+              },
+            };
+            discut.push(message);
+            setValue("")
+          }
+        }}
+      >
+        <AiOutlineSend />
+      </button>
+    </div>
+  );
 }
