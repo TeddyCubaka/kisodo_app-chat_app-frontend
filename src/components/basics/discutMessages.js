d86ec3066e81f4204083181ce72fe26c8e25b351
Teddy Cubaka <teddycubaka96@gmail.com>
2022-10-10 21:51:54 +0100

    message bubble styled

--- src/components/basics/discutMessages.js
+++ src/components/basics/discutMessages.js
@@ -1,13 +1,12 @@
-import React, { useRef } from "react";
+import React from "react";
 import Message from "./message";
 
 export default function DiscutMessages() {
-  const divRef = useRef(null);
 
   return (
-    <div className="discut_msg" ref={divRef}>
-      <Message />
-      <Message />
+    <div className="discut_msg">
+        <Message bulle={"message_right"} position={"flex_end-r"} />
+        <Message bulle={"message_left"} position={"flex_start-r"} />
     </div>
   );
 }
--- src/components/basics/message.js
+++ src/components/basics/message.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function Message (){
+export default function Message ({ position, bulle }){
     return (
-        <div className="message">
-            <div className="message_bulle">content</div>
+        <div className={`message ${position}`}>
+            <div className={`${bulle}`}  >cofoufiyuevfvye ufuyevuyevyuevyuev y    uevuyevufvuyaevfefu evyueeyvyufyuefyueyevyueeyuenten jkbfe fe f ieu ie ie fei fei fei fei e fei feiut</div>
             <div className="small">data</div>
         </div>
     )
