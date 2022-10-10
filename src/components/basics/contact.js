f29dc7d9377be8b551a3983269d2ec6a5be47b50
Teddy Cubaka <teddycubaka96@gmail.com>
2022-10-10 12:07:06 +0100

    display contact in home component

--- src/components/basics/contact.js
+++ src/components/basics/contact.js
@@ -1,15 +1,20 @@
 import React from "react";
 
 
-export default function Contact (){
+export default function Contact ({name, message, image}){
     return (
         <div className="contact_card">
             <div className="contact_img">
-                <img src="https://cdn1.iconfinder.com/data/icons/circle-flats/170/contacts-512.png" alt=""/>
+                {image ? <img src={image} alt={`Image de ${name}`} />
+                 : <img src="https://cdn1.iconfinder.com/data/icons/circle-flats/170/contacts-512.png" alt=""/>
+                 }
             </div>
             <div className="contact_info">
-                <div className="strong">Name</div>
-                <div className="small">Message</div>
+                <div className="strong"> {name} </div>
+                {message ? 
+                <div className="small">{ message }</div>
+                : <div className="small">No message here</div>
+                }
             </div>
         </div>
     )
