f67696ea2dc8b769528ba43e8f12de7f8121bed1
Teddy Cubaka <teddycubaka96@gmail.com>
2022-10-10 11:29:25 +0100

    add first grid style to arrange the home page

--- src/components/bigs/home.js
+++ src/components/bigs/home.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Home (){
     return (
-        <div>This will be the home</div>
+        <div className="home radius">This will be the home</div>
     )
 }
