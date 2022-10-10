940dc4b829f11e08d52548aa269d4740a46ca2c7
Teddy Cubaka <teddycubaka96@gmail.com>
2022-10-10 12:41:24 +0100

    add the discussion info component

---
+++ src/components/basics/discutInfo.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function DiscutInfo (){
+    return (
+        <div className="discut_info">
+            <div className="img_card">
+                <img src="https://cdn1.iconfinder.com/data/icons/circle-flats/170/contacts-512.png" alt="" />
+            </div>
+            <div className="margin_x-10 content_center width-max">
+                <div className="strong">Name of contact</div>
+                <div className="small">Online</div>
+            </div>
+        </div>
+    )
+}
