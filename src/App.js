2b9375bed43cb700de1cf9aed60484f55a9e9230
Teddy Cubaka <teddycubaka96@gmail.com>
2022-10-10 12:20:49 +0100

    add the discussion context for orginise my discussions on the app

--- src/App.js
+++ src/App.js
@@ -5,19 +5,24 @@ import "./AppComponents.css"
 import Navigation from "./components/bigs/navigation";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Search from "./components/bigs/search";
+import discussionContext from "./contexts/discussion"
+import { useContext, useState } from "react";
 
 function App() {
+  const [discut, setDiscut] = useState(useContext(discussionContext))
   return (
-    <Router>
-      <div className="App">
-        <Navigation />
-        <div className="margin">
-          <Search />
-          <Home />
+    <discussionContext.Provider value={{discut, setDiscut}}>
+      <Router>
+        <div className="App">
+          <Navigation />
+          <div className="margin">
+            <Search />
+            <Home />
+          </div>
+          <Discussion />
         </div>
-        <Discussion />
-      </div>
-    </Router>
+      </Router>
+    </discussionContext.Provider>
   );
 }
 
--- src/components/bigs/home.js
+++ src/components/bigs/home.js
@@ -1,26 +1,35 @@
 import React, { useEffect, useState } from "react";
 import Contact from "../basics/contact";
 
-export default function Home (){
-    const [contact, setContact] = useState([])
-    useEffect(()=>{
-        fetch('http://localhost:3000/api/user')
-            .then(data => data.json()
-                .then(res => setContact(res))
-                .catch(err => console.log(err))
-            )
-            .catch(err => console.log(err))
-    }, [])
-    return (
-        <div className="home radius">
-            <h3>Recent</h3>
-            {
-                contact.length > 1 ? 
-                contact.map((cont)=>(
-                    <Contact name={`${cont.firstName} ${cont.secondName}`} message={false} />
-                ))
-                : <div>No contact here</div>
-            }
-        </div>
-    )
+export default function Home() {
+  const [contact, setContact] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/api/user")
+      .then((data) =>
+        data
+          .json()
+          .then((res) => {
+            setContact(res);
+            console.log(res);
+          })
+          .catch((err) => console.log(err))
+      )
+      .catch((err) => console.log(err));
+  }, []);
+  return (
+    <div className="home radius">
+      <h3>Recent</h3>
+      {contact.length > 1 ? (
+        contact.map((cont) => (
+          <Contact
+            name={`${cont.firstName} ${cont.secondName}`}
+            message={false}
+            key={cont._id}
+          />
+        ))
+      ) : (
+        <div>No contact here</div>
+      )}
+    </div>
+  );
 }
