ad9d9718494b85dc27e0d70c5bd8b284b0b1c03d
Teddy Cubaka <teddycubaka96@gmail.com>
2022-10-10 11:46:52 +0100

    add contact component with style

---
+++ src/components/basics/contact.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+export default function Contact (){
+    return (
+        <div className="contact_card">
+            <div className="contact_img">
+                <img src="https://cdn1.iconfinder.com/data/icons/circle-flats/170/contacts-512.png" alt=""/>
+            </div>
+            <div className="contact_info">
+                <div className="strong">Name</div>
+                <div className="small">Message</div>
+            </div>
+        </div>
+    )
+}
--- src/components/bigs/home.js
+++ src/components/bigs/home.js
@@ -1,7 +1,11 @@
 import React from "react";
+import Contact from "../basics/contact";
 
 export default function Home (){
     return (
-        <div className="home radius">This will be the home</div>
+        <div className="home radius">
+            <h3>Recent</h3>
+            <Contact />
+        </div>
     )
 }
