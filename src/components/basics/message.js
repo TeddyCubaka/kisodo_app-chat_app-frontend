5660f4cf3e0eb1e39ba9c2a66634fc4b58ed508c
Teddy Cubaka <teddycubaka96@gmail.com>
2022-10-10 21:57:02 +0100

    clean the app.js codes

--- src/components/basics/message.js
+++ src/components/basics/message.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Message ({ position, bulle }){
     return (
         <div className={`message ${position}`}>
-            <div className={`${bulle}`}  >cofoufiyuevfvye ufuyevuyevyuevyuev y    uevuyevufvuyaevfefu evyueeyvyufyuefyueyevyueeyuenten jkbfe fe f ieu ie ie fei fei fei fei e fei feiut</div>
+            <div className={`${bulle}`}  >Hello</div>
             <div className="small">data</div>
         </div>
     )
