3f75c36d2eac22fcaab59803103a8aee734ef8fc
Teddy Cubaka <teddycubaka96@gmail.com>
2022-10-11 16:23:13 +0100

    display messages in terms of userId

--- src/components/basics/message.js
+++ src/components/basics/message.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-export default function Message ({ position, bulle, content, date }){
+export default function Message ({ position, content, date, bulle }){
     return (
-        <div className={`message ${position}`}>
-            <div className={`${bulle}`}  > {content} </div>
+        <div className={`message ${position}`}
+        onClick={()=>{
+            console.log(date)
+        }}
+        >
+            <div className={bulle}  > {content} </div>
             <div className="smaller"> {date} </div>
         </div>
     )
