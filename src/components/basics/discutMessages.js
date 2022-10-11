55d39ce75fa8437c40e1f53488c24d76c382a4d8
Teddy Cubaka <teddycubaka96@gmail.com>
2022-10-11 12:51:55 +0100

    fetch and display message in the front

--- src/components/basics/discutMessages.js
+++ src/components/basics/discutMessages.js
@@ -1,12 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+// import discussionContext from "../../contexts/discussion";
 import Message from "./message";
 
 export default function DiscutMessages() {
+  // const { discut } = useContext(discussionContext);
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/message")
+      .then((res) => {
+        res.json().then((data) => {
+          console.log(data)
+          setMessages(data.message)
+        })
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div className="discut_msg">
-        <Message bulle={"message_right"} position={"flex_end-r"} />
-        <Message bulle={"message_left"} position={"flex_start-r"} />
+      <Message
+        bulle={"message_right"}
+        position={"flex_end-r"}
+        content={"Hello"}
+        date={"10-10-2022 10:10"}
+      />
+      <Message
+        bulle={"message_left"}
+        position={"flex_start-r"}
+        content={"Hello Babe"}
+        date={"10-10-2022 10:10"}
+      />
+      {messages.length > 1 ? (
+        messages.map((data, index) => (
+          <Message
+            key={data.id}
+            bulle={"message_right"}
+            position={"flex_end-r"}
+            content={data.content}
+            date={data.sendDate}
+          />
+        ))
+      ) : (
+        <div>No message here</div>
+      )}
     </div>
   );
 }
--- src/components/basics/message.js
+++ src/components/basics/message.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function Message ({ position, bulle }){
+export default function Message ({ position, bulle, content, date }){
     return (
         <div className={`message ${position}`}>
-            <div className={`${bulle}`}  >Hello</div>
-            <div className="small">data</div>
+            <div className={`${bulle}`}  > {content} </div>
+            <div className="small"> {date} </div>
         </div>
     )
 }
