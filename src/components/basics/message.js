63bfa06bafb02474aa6d9934d082bc498c81fe87
Teddy Cubaka <teddycubaka96@gmail.com>
2022-10-12 11:54:34 +0100

    arrange style of message card, fit the height to content height

--- src/components/basics/message.js
+++ src/components/basics/message.js
@@ -2,12 +2,8 @@ import React from "react";
 
 export default function Message ({ position, content, date, bulle }){
     return (
-        <div className={`message ${position}`}
-        onClick={()=>{
-            console.log(date)
-        }}
-        >
-            <div className={bulle}  > {content} </div>
+        <div className={`message ${position}`}>
+            <div className={bulle}> {content} </div>
             <div className="smaller"> {date} </div>
         </div>
     )
