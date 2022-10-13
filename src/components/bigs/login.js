36ca171e7ee319e6cbc197188f85eee14ce6585a
Teddy Cubaka <teddycubaka96@gmail.com>
2022-10-13 19:36:09 +0100

    sign up a user

--- src/components/bigs/login.js
+++ src/components/bigs/login.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
@@ -5,22 +6,32 @@ export default function Login() {
   const [mail, setMail] = useState("");
   const [password, setPassword] = useState("");
   const [msg, setMsg] = useState("");
+  const [pwdSize, setPwdSize] = useState(0);
+  const [mailSize, setMailSize] = useState(0);
+  const [CorrectMail, setCorrectMail] = useState("");
+  const [nicePwdSize, setNicePwdSize] = useState("");
   return (
     <div>
       <fieldset>
         <h2>What's up ?</h2>
         {msg == "" ? false : <div color="red"> {msg} </div>}
-        <label>Enter your mail address</label>
+        <label>
+          Enter your mail address {mailSize > 1 ? mailSize : false}{" "}
+          {CorrectMail}{" "}
+        </label>
         <input
           type="mail"
           name="email"
           placeholder="Enter your email address"
           required
           onChange={(e) => {
-            setMail(e.target.value);
+            setMailSize(e.target.value.length);
+            if (mailSize > 10) setMail(e.target.value);
           }}
         />
-        <label>Enter your password</label>
+        <label>
+          Enter your password {pwdSize > 1 ? pwdSize : false} {nicePwdSize}
+        </label>
         <input
           type="password"
           name="password"
@@ -28,6 +39,7 @@ export default function Login() {
           required
           onChange={(e) => {
             setPassword(e.target.value);
+            setPwdSize(mailSize + 1);
           }}
         />
         <input
@@ -35,19 +47,26 @@ export default function Login() {
           name="logIn"
           value="log in"
           onClick={() => {
+            mailSize < 10
+              ? setCorrectMail("Check your mail")
+              : setCorrectMail("");
+            pwdSize < 8
+              ? setNicePwdSize("Too short passeword change it plz")
+              : setNicePwdSize("nice password");
             if (mail == "" || password == "") {
-              setMsg("Please enter your email address and password");
+              setMsg("The mail or the password is invalid");
             } else if (mail.endsWith("gmail.com") == false) {
               setMsg("A gmail plz");
             } else {
-              console.log(mail, password);
+              setMsg("");
+              axios();
             }
           }}
         />
       </fieldset>
       <div>
         <div>
-          You don't have an account ? <Link to="login">Sign Up ?</Link>{" "}
+          You don't have an account ? <Link to="/signup">Sign Up ?</Link>{" "}
         </div>
       </div>
     </div>
