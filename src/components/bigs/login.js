8ca6e5cdf1368e3d2d4cede690750b7fad092877
Teddy Cubaka <teddycubaka96@gmail.com>
2022-10-14 12:42:45 +0100

    user can sign up and login

--- src/components/bigs/login.js
+++ src/components/bigs/login.js
@@ -3,72 +3,76 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Login() {
-  const [mail, setMail] = useState("");
-  const [password, setPassword] = useState("");
-  const [msg, setMsg] = useState("");
-  const [pwdSize, setPwdSize] = useState(0);
-  const [mailSize, setMailSize] = useState(0);
-  const [CorrectMail, setCorrectMail] = useState("");
-  const [nicePwdSize, setNicePwdSize] = useState("");
-  return (
-    <div>
-      <fieldset>
-        <h2>What's up ?</h2>
-        {msg == "" ? false : <div color="red"> {msg} </div>}
-        <label>
-          Enter your mail address {mailSize > 1 ? mailSize : false}{" "}
-          {CorrectMail}{" "}
-        </label>
-        <input
-          type="mail"
-          name="email"
-          placeholder="Enter your email address"
-          required
-          onChange={(e) => {
-            setMailSize(e.target.value.length);
-            if (mailSize > 10) setMail(e.target.value);
-          }}
-        />
-        <label>
-          Enter your password {pwdSize > 1 ? pwdSize : false} {nicePwdSize}
-        </label>
-        <input
-          type="password"
-          name="password"
-          placeholder="Enter your password"
-          required
-          onChange={(e) => {
-            setPassword(e.target.value);
-            setPwdSize(mailSize + 1);
-          }}
-        />
-        <input
-          type="button"
-          name="logIn"
-          value="log in"
-          onClick={() => {
-            mailSize < 10
-              ? setCorrectMail("Check your mail")
-              : setCorrectMail("");
-            pwdSize < 8
-              ? setNicePwdSize("Too short passeword change it plz")
-              : setNicePwdSize("nice password");
-            if (mail == "" || password == "") {
-              setMsg("The mail or the password is invalid");
-            } else if (mail.endsWith("gmail.com") == false) {
-              setMsg("A gmail plz");
-            } else {
-              setMsg("");
-              axios();
-            }
-          }}
-        />
-      </fieldset>
-      <div>
-        <div>
-          You don't have an account ? <Link to="/signup">Sign Up ?</Link>{" "}
+    const [mail, setMail] = useState("");
+    const [password, setPassword] = useState("");
+    const [token, setToken] = useState("");
+    const [loader, setLoader] = useState("");
+    return (
+        <div className="mum_card">
+            <h2>What's up ?</h2>
+            <fieldset className="fieldset_of_connexion">
+                <div className={loader}></div>
+                <label>Enter your mail address</label>
+                <input
+                    type="mail"
+                    name="email"
+                    placeholder="Enter your email address"
+                    required
+                    onChange={(e) => {
+                        setMail(e.target.value);
+                    }}
+                />
+                <label>Enter your password</label>
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Enter your password"
+                    required
+                    onChange={(e) => {
+                        setPassword(e.target.value);
+                    }}
+                />
+                <input
+                    type="button"
+                    name="logIn"
+                    value="log in"
+                    onClick={() => {
+                        if (mail !== "" || password !== "") {
+                            setLoader("loader");
+                            const myData = {
+                                mail: mail,
+                                password: password,
+                            };
+                            console.log(myData);
+                            axios({
+                                method: "post",
+                                url: "http://localhost:3000/api/user/login",
+                                data: myData,
+                                headers: { "Content-Type": "application/json" },
+                            })
+                                .then((res) => {
+                                    console.log(res.data.token);
+                                    setToken(res.data.token);
+                                    localStorage.setItem(
+                                        "token",
+                                        res.data.token,
+                                    );
+                                    setLoader("");
+                                    // window.location = "/home";
+                                })
+                                .catch((err) =>
+                                    console.log(err, "jafuefoefuoeogfef"),
+                                );
+                        }
+                    }}
+                />
+            </fieldset>
+            <div>
+                <div>
+                    You don't have an account ?{" "}
+                    <Link to="/signup">Sign Up ?</Link>{" "}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
--- src/components/bigs/signup.js
+++ src/components/bigs/signup.js
@@ -13,96 +13,102 @@ export default function Signup() {
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     return (
-        <div>
+        <div className="mum_card">
             <h2>Did you want to join us ?</h2>
-            {msg == "" ? false : <div style={{ color: "red" }}> {msg} </div>}
-            <label>What is your name ?</label>
-            <input
-                type="text"
-                name="firstName"
-                placeholder="Enter your name"
-                required
-                onChange={(e) => {
-                    setFirstName(e.target.value);
-                }}
-            />
-            <label>What's your secondName ?</label>
-            <input
-                type="text"
-                name="secondName"
-                placeholder="Enter your send name"
-                required
-                onChange={(e) => {
-                    setLastName(e.target.value);
-                }}
-            />
-            <label>
-                Enter your mail address {mailSize > 1 ? mailSize : false}{" "}
-                {CorrectMail}{" "}
-            </label>
-            <input
-                type="mail"
-                name="email"
-                placeholder="Enter your email address"
-                required
-                onChange={(e) => {
-                    setMailSize(e.target.value.length);
-                    if (mailSize > 10) setMail(e.target.value);
-                }}
-            />
-            <label>
-                What's your password {pwdSize > 1 ? pwdSize : false}{" "}
-                {nicePwdSize}
-            </label>
-            <input
-                type="password"
-                name="password"
-                placeholder="Enter your password"
-                required
-                onChange={(e) => {
-                    setPassword(e.target.value);
-                    if (nicePwdSize > 8) setPwdSize(mailSize + 1);
-                }}
-            />
-            <input
-                type="button"
-                name="signUp"
-                value="Sign up"
-                onClick={() => {
-                    mailSize < 10
-                        ? setCorrectMail("Check your mail")
-                        : setCorrectMail("");
-                    pwdSize < 8
-                        ? setNicePwdSize("Too short passeword change it plz")
-                        : setNicePwdSize("nice password");
-                    if (mail == "" || password == "") {
-                        setMsg("The mail or the password is invalid");
-                    } else if (mail.endsWith("gmail.com") == false) {
-                        setMsg("A gmail plz");
-                    } else {
-                        setMsg("");
-                        const myData = {
-                            firstName: firstName,
-                            secondName: lastName,
-                            mail: mail,
-                            password: password,
-                        };
+            <fieldset className="fieldset_of_connexion">
+                {msg == "" ? (
+                    false
+                ) : (
+                    <div style={{ color: "red" }}> {msg} </div>
+                )}
+                <label>What is your name ?</label>
+                <input
+                    type="text"
+                    name="firstName"
+                    placeholder="Enter your name"
+                    required
+                    onChange={(e) => {
+                        setFirstName(e.target.value);
+                    }}
+                />
+                <label>What's your secondName ?</label>
+                <input
+                    type="text"
+                    name="secondName"
+                    placeholder="Enter your send name"
+                    required
+                    onChange={(e) => {
+                        setLastName(e.target.value);
+                    }}
+                />
+                <label>
+                    Enter your mail address {mailSize > 1 ? mailSize : false}{" "}
+                    {CorrectMail}{" "}
+                </label>
+                <input
+                    type="mail"
+                    name="email"
+                    placeholder="Enter your email address"
+                    required
+                    onChange={(e) => {
+                        setMailSize(e.target.value.length);
+                        if (mailSize > 10) setMail(e.target.value);
+                    }}
+                />
+                <label>
+                    What's your password {pwdSize > 1 ? pwdSize : false}{" "}
+                    {nicePwdSize}
+                </label>
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Enter your password"
+                    required
+                    onChange={(e) => {
+                        setPassword(e.target.value);
+                        if (nicePwdSize > 8) setPwdSize(mailSize + 1);
+                    }}
+                />
+                <input
+                    type="button"
+                    name="signUp"
+                    value="Sign up"
+                    onClick={() => {
+                        mailSize < 10
+                            ? setCorrectMail("Check your mail")
+                            : setCorrectMail("");
+                        pwdSize < 8
+                            ? setNicePwdSize(
+                                  "Too short passeword change it plz",
+                              )
+                            : setNicePwdSize("nice password");
+                        if (mail == "" || password == "") {
+                            setMsg("The mail or the password is invalid");
+                        } else if (mail.endsWith("gmail.com") == false) {
+                            setMsg("A gmail plz");
+                        } else {
+                            setMsg("");
+                            const myData = {
+                                firstName: firstName,
+                                secondName: lastName,
+                                mail: mail,
+                                password: password,
+                            };
 
-                        // const obj = JSON.stringify(myData, false, "\t");
-
-                        axios({
-                            method: "post",
-                            url: "http://localhost:3000/api/user/signup",
-                            data: myData,
-                            headers: { "Content-Type": "application/json" },
-                        })
-                            .then((res) => console.log(res))
-                            .catch((err) =>
-                                console.log(err, "jafuefoefuoeogfef"),
-                            );
-                    }
-                }}
-            />
+                            axios({
+                                method: "post",
+                                url: "http://localhost:3000/api/user/signup",
+                                data: myData,
+                                headers: { "Content-Type": "application/json" },
+                            })
+                                .then((res) => console.log(res))
+                                .catch((err) =>
+                                    console.log(err, "jafuefoefuoeogfef"),
+                                );
+                        }
+                    }}
+                />
+            </fieldset>
         </div>
     );
 }
