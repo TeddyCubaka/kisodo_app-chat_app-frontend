8cd9f3b40fe905af4336a6c302a69fec33301bdf
Teddy Cubaka <teddycubaka96@gmail.com>
2022-10-17 00:13:17 +0100

    create a new discussion in the front

--- src/components/basics/satartConverse.js
+++ src/components/basics/satartConverse.js
@@ -1,22 +1,93 @@
-import React from "react";
+import React, { useContext } from "react";
 import { MdOutlineContactMail } from "react-icons/md";
 import { AiOutlineCamera } from "react-icons/ai";
+import discussionContext from "../../contexts/discussion";
+import axios from "axios";
 
 export default function StartConverse() {
+    const { actualDiscussion, freind, me } = useContext(discussionContext);
     return (
-        <div>
+        <div className="text_zone">
             <div className="text_input">
                 <textarea
                     spellCheck="true"
-                    value={"start converse xith him"}
+                    placeholder="Start converse with this person"
                 ></textarea>
                 <div>
                     <AiOutlineCamera size="30px" />
                 </div>
             </div>
-            <button>
+            <button
+                onClick={() => {
+                    if (prompt("sure ?", "oui")) {
+                        axios({
+                            method: "post",
+                            url: "http://localhost:3000/api/discussion/",
+                            headers: {
+                                "Content-Type": "application/json",
+                                Authorization:
+                                    "Bearer " + localStorage.getItem("token"),
+                            },
+                            data: {
+                                isGroup: false,
+                                membres: [
+                                    {
+                                        userId: freind.userId,
+                                        fullName: freind.fullName,
+                                        image: freind.image,
+                                        biography: freind.biography,
+                                    },
+                                    {
+                                        userId: me.userId,
+                                        fullName:
+                                            me.firstName + " " + me.secondName,
+                                        image: me.image,
+                                        biography: me.biography,
+                                    },
+                                ],
+                            },
+                        })
+                            .then((res) => console.log(res.data))
+                            .catch((err) => console.log(err));
+                    } else {
+                        console.log("nan");
+                    }
+                }}
+            >
                 <MdOutlineContactMail />
             </button>
         </div>
     );
 }
+
+//     if (!actualDiscussion.userId) {
+//         axios({
+//             method: "post",
+//             url: "http://localhost:3000/api/discussion/",
+//             headers: {
+//                 "Content-Type": "application/json",
+//                 Authorization:
+//                     "Bearer " + localStorage.getItem("token"),
+//             },
+//             data: {
+//                 isGroup: false,
+//                 membres: [
+//                     {
+//                         userId: freind.userId,
+//                         fullName: freind.fullName,
+//                         image: freind.image,
+//                         biography: freind.biography,
+//                     },
+//                     {
+//                         userId: me.userId,
+//                         fullName:
+//                             me.firstName + " " + me.secondName,
+//                         image: me.image,
+//                         biography: me.biography,
+//                     },
+//                 ],
+//             },
+//         })
+//             .then((res) => console.log(res))
+//             .catch((err) => console.log(err));
+//     }
