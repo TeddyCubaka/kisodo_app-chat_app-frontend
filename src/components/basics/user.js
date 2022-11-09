6525fcf022360498edef7f28b1637e47343c818c
teddy cubaka <teddycubaka96@gmail.com>
2022-11-09 14:18:54 +0100

    create the pop-up to display and update the current user

--- src/components/basics/user.js
+++ src/components/basics/user.js
@@ -1,19 +1,93 @@
 import React, { useState } from "react";
 import { TbPoint } from "react-icons/tb";
 import avatar from "../../images/avatar.png";
+import { MdEdit } from "react-icons/md";
 
 export default function User(me) {
   const [updateProfile, setUpdateProfile] = useState(false);
+  const [newAvatar, setAvatar] = useState(me.me.image ? me.me.image : avatar);
+
+  const ModifyProfile = () => {
+    const [state, setState] = useState(false);
+    return state === false ? (
+      <div className="profile_static margin_x-10">
+        <div className="strongest">
+          Modifié le profile{" "}
+          <button
+            onClick={() => {
+              setState(true);
+            }}
+          >
+            <MdEdit />
+          </button>
+        </div>
+        <div>
+          Nombre des contacts : <strong>30</strong>{" "}
+        </div>
+        <div>
+          Nombre des messages : <strong>6576</strong>{" "}
+        </div>
+      </div>
+    ) : (
+      <form className="form_profile margin_x-20">
+        <label>Nom</label>
+        <input type="text" value={me.me.firstName} />
+        <label>Post-nom</label>
+        <input type="text" value={me.me.secondName} />
+        <label>Bio</label>
+        <input type="text" value={me.me.biography} />
+        <button
+          onClick={() => {
+            setState(false);
+          }}
+        >
+          valider
+        </button>
+      </form>
+    );
+  };
   return updateProfile === true ? (
-    <div
-      onClick={() => {
-        setUpdateProfile(false);
-      }}
-      className="absolute profile"
-    >
-      <div>
-        {" "}
-        {me.me.firstName} {me.me.secondName}{" "}
+    <div>
+      <div className="profile">
+        <button
+          onClick={() => {
+            setUpdateProfile(false);
+          }}
+        >
+          X
+        </button>
+        <div
+          className="profile_form"
+          onClick={() => {
+            setUpdateProfile(true);
+          }}
+        >
+          <div>
+            <div className="margin_y-20_10 content_center big_img_card">
+              <img src={newAvatar} alt="" />
+              <div className="edit_photo">
+                <input
+                  type="file"
+                  accept="image/*"
+                  className="input_file"
+                  onChange={(e) => {
+                    if (prompt("changer la photo de profile ?", "oui")) {
+                      setAvatar(URL.createObjectURL(e.target.files[0]));
+                    } else {
+                      alert("nope");
+                    }
+                  }}
+                />
+                <MdEdit size="30px" color="white" />
+              </div>
+              <div className="strongest">
+                {me.me.firstName} {me.me.secondName}{" "}
+              </div>
+            </div>
+            <div> {me.me.bio} </div>
+          </div>
+          <ModifyProfile />
+        </div>
       </div>
     </div>
   ) : (
@@ -24,7 +98,7 @@ export default function User(me) {
           setUpdateProfile(true);
         }}
       >
-        <img src={avatar} alt="" />
+        <img src={newAvatar} alt="" />
         {me.me.onLine ? (
           <TbPoint size="15px" color="green" />
         ) : (
