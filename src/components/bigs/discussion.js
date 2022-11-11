7f7a218decb91d750b4f9738c3bdfd1e7e7acbd9
teddy cubaka <teddycubaka96@gmail.com>
2022-11-11 12:32:08 +0100

    display an alternative illustration if there are not a discussion choosen

--- src/components/bigs/discussion.js
+++ src/components/bigs/discussion.js
@@ -6,12 +6,30 @@ import StartConverse from "../basics/satartConverse";
 import TextZone from "../basics/textZone";
 
 export default function Discussion() {
-  const { actualDiscussion, allMember } = useContext(discussionContext);
+  const { actualDiscussion, allMember, freind, me } =
+    useContext(discussionContext);
+
   return (
     <div className="discussion radius margin bloc_with_shaddow">
-      <DiscutInfo />
-      <DiscutMessages member={allMember} />
-      {actualDiscussion.discussionId ? <TextZone /> : <StartConverse />}
+      {freind.userId ? (
+        <>
+          <DiscutInfo />
+          <DiscutMessages member={allMember} />
+          {actualDiscussion.discussionId ? <TextZone /> : <StartConverse />}
+        </>
+      ) : (
+        <div className="choose_conversation_card">
+          <h1>
+            Welcome {me.firstName ? `${me.firstName} ${me.secondName}` : " "}{" "}
+          </h1>
+          <div className="image_d_accueil">
+            <img
+              src="https://i0.wp.com/blog.fastandfresh.fr/wp-content/uploads/2019/08/Conception-de-sites-web-page-daccueil.png?resize=989%2C640&ssl=1"
+              alt="illustration d'accueil"
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/bigs/home.js
+++ src/components/bigs/home.js
@@ -12,10 +12,12 @@ export let socket =
 export default function Home() {
   const { me } = useContext(discussionContext);
   useEffect(() => {
-    if (me.userId) socket.emit("online", me);
-    socket.on("userOnline", (users) => {
-      me.onLine = true;
-    });
+    if (me.userId) {
+      socket.emit("online", me);
+      socket.on("userOnline", (users) => {
+        me.onLine = true;
+      });
+    }
   }, [me]);
 
   return (
